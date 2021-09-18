4e006fdbad67b07ebfa4a8e2984b6dc0190db606
Justin Lettau <justin.b.lettau@gmail.com>
2021-09-17 23:03:47 -0700

    fix(docs): home page links

--- docs/src/pages/Home.vue
+++ docs/src/pages/Home.vue
@@ -3,9 +3,13 @@
     <h1>Packages</h1>
     <nav>
       <v-list>
-        <v-list-item is="router-link" to="/common"> Common </v-list-item>
-        <v-list-item is="router-link" to="/forms"> Forms </v-list-item>
-        <v-list-item is="router-link" to="/aws-auth"> AWS Auth </v-list-item>
+        <v-list-item is="router-link" to="/common/accordion">
+          Common
+        </v-list-item>
+        <v-list-item is="router-link" to="/forms/checkbox"> Forms </v-list-item>
+        <v-list-item is="router-link" to="/aws-auth/sign-in">
+          AWS Auth
+        </v-list-item>
       </v-list>
     </nav>
   </main>
