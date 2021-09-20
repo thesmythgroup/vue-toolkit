a11bb70539c3b699c21ea70f617d3f645dc5612f
Justin Lettau <justin.b.lettau@gmail.com>
2021-09-20 11:17:57 -0700

    feat(popover): expose methods to default slot

--- docs/src/pages/common/Popover.vue
+++ docs/src/pages/common/Popover.vue
@@ -7,7 +7,12 @@
         <v-button @click="toggle">Open Popover</v-button>
       </template>
 
-      <p class="m-0 p-4">Popover content</p>
+      <template #default="{ close }">
+        <div class="p-4">
+          <p class="m-0 mb-4">Popover content</p>
+          <v-button @click="close">Manually close</v-button>
+        </div>
+      </template>
     </v-popover>
   </section>
 </template>
--- packages/common/src/components/popover/Popover.vue
+++ packages/common/src/components/popover/Popover.vue
@@ -1,45 +1,58 @@
 <template>
-  <div class="popover">
-    <slot name="trigger" :toggle="toggle" />
+  <div class="popover" ref="root">
+    <slot name="trigger" v-bind="{ isOpen, toggle, open, close }" />
 
     <div class="popover__content" v-if="isOpen">
-      <slot></slot>
+      <slot v-bind="{ isOpen, toggle, open, close }" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import {
+  defineComponent,
+  ref,
+  onMounted,
+  onUnmounted,
+} from '@vue/composition-api';
 
 export default defineComponent({
   name: 'v-popover',
-  data() {
-    return {
-      isOpen: false,
-    };
-  },
-  created() {
-    document.body.addEventListener('click', this.onBodyClick);
-  },
-  destroyed() {
-    document.body.removeEventListener('click', this.onBodyClick);
-  },
-  methods: {
-    onBodyClick(event: Event) {
+  setup() {
+    const isOpen = ref(false);
+    const root = ref<HTMLElement | null>(null);
+
+    const toggle = () => (isOpen.value = !isOpen.value);
+    const open = () => (isOpen.value = true);
+    const close = () => (isOpen.value = false);
+
+    const handleBodyClick = (event: Event) => {
       const { target } = event;
 
-      if (!this.isOpen || !target) {
+      if (!isOpen.value || !target) {
         return;
       }
 
-      if (!(this.$el === target || this.$el.contains(target as HTMLElement))) {
+      if (
+        !(root.value === target || root.value.contains(target as HTMLElement))
+      ) {
         // outside click
-        this.isOpen = false;
+        isOpen.value = false;
       }
-    },
-    toggle() {
-      this.isOpen = !this.isOpen;
-    },
+    };
+
+    onMounted(() => document.body.addEventListener('click', handleBodyClick));
+    onUnmounted(() =>
+      document.body.removeEventListener('click', handleBodyClick)
+    );
+
+    return {
+      root,
+      isOpen,
+      toggle,
+      open,
+      close,
+    };
   },
 });
 </script>
