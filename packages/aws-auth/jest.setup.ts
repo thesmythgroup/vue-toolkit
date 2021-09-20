e2a2d637a0de6c0e73d1c110e0440fba0dbeb1f1
Justin Lettau <justin.b.lettau@gmail.com>
2021-09-20 10:14:07 -0700

    chore: update package imports in tests

--- packages/aws-auth/jest.setup.ts
+++ packages/aws-auth/jest.setup.ts
@@ -1,9 +1,9 @@
 import 'regenerator-runtime/runtime';
 
-import Common from '@vue-toolkit/common';
-import Forms from '@vue-toolkit/forms';
 import Vue from 'vue';
 
+import Common from '../common/src';
+import Forms from '../forms/src';
 import AwsAuth from './src';
 
 Vue.use(Common);
